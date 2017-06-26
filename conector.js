aba3c34201018ccc7552ee3dd8b5dcac5677b9c9
Usuario <Usuario>
2017-06-26 11:30:26 -0500

    se muestran datos en la tabla y con estilo

--- conector.js
+++ conector.js
@@ -18,7 +18,20 @@ function mostrar(){
 
 function mostrar_resultados(resultados){
     $.each(resultados, function(index, valor){
-        console.log(index)
-        console.log(valor)
+       // console.log(index)
+        //console.log(valor)
+        
+        var row = '<tr>'
+        row += '<td>' + valor[0] + '</td>'
+        row += '<td>' + valor[1] + '</td>'
+        row += '<td>' + valor[2] + '</td>'
+        row += '<td>' + valor[3] + '</td>'
+        row += '<td>' + valor[4] + '</td>'
+        row += '<td>' + valor[5] + '</td>'
+        row += '<td>' + valor[6] + '</td>'
+        row += '</tr>'
+        
+        $('#usuariosTable').append(row)
+        
     })
 }
